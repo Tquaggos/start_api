12285eb93a931616b3823c6681991534598d3970
quaggosxr <arthur.quaggos200@gmail.com>
2026-06-02 10:54:33 -0300

    Add files via upload

---
+++ biolab/biolab/src/main/java/com/example/biolab/entities/Usuario.java
@@ -0,0 +1,55 @@
+package com.example.biolab.entities;
+
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    private String nome;
+    private String email;
+
+    public Usuario(){
+    }
+
+
+    public Usuario(long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
